eede59322d7245d8fe1e5585d771bc81d08f3dee
Wannes Salomé <mail@wannessalome.nl>
2020-03-21 01:47:02 +0100

    disable no-use-before-define for functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
     'comma-dangle': ['warn', 'always-multiline'],
     'import/prefer-default-export': 'off',
     'no-console': 'off',
     'no-empty-function': 'off',
+    'no-use-before-define': ['off'],
     'import/no-extraneous-dependencies': [
       'error',
       { devDependencies: ['**/*.test.tsx', '**/test/**/*'] },
