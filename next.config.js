2758ac7fb50f9a7d45dfe034a4cc74a54c783adb
Wannes Salomé <mail@wannessalome.nl>
2020-03-24 17:07:53 +0100

    expose env variables to client-side

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
 const { resolve } = require('path');
-const dotEnvResult = require('dotenv').config();
-
-const parsedVariables = dotEnvResult.parsed || {};
-const env = {};
-
-for (const key of Object.keys(parsedVariables)) {
-  env[key] = process.env[key];
-}
 
 module.exports = {
-  env,
+  env: {
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
+    SHOPIFY_COLLECTION_ID: process.env.SHOPIFY_COLLECTION_ID,
+    SHOPIFY_DOMAIN: process.env.SHOPIFY_DOMAIN,
+    SHOPIFY_TOKEN: process.env.SHOPIFY_TOKEN,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    STRIPE_SECRET: process.env.STRIPE_SECRET,
+  },
   webpack(config, { dev, isServer }) {
     config.resolve.alias['#root'] = resolve(__dirname, 'src');
-
     return config;
   },
 };
